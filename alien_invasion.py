b9af58815a3a4b082f3a1b09d7cb6d103f71569d
bugminsterhamilton-cloud <bugminsterhamilton@gmail.com>
2025-11-05 18:05:13 +0300

    added up/down  mouved

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,7 +12,11 @@ class AlienInvasion():
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)  # запуск в полноэкранном режиме
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
+        # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
         self.ship = Ship(self)
 
@@ -35,17 +39,41 @@ class AlienInvasion():
                 sys.exit()
             # уезжает за пределы экрана и не останавливается
             elif event.type == pygame.KEYDOWN:  # начал отсюда 5.11.25 (движение работает)
-                        # right:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True  # ВКЛЮЧАЕТ движение
-                        # left:
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)  #???
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False  # ВЫКЛЮЧАЕТ движение
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """реагигирует на нажатие клавиш"""
+        # right:
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True  # ВКЛЮЧАЕТ движение
+        # left:
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_ESCAPE:  # добавил кнопки выхода из игры
+            sys.exit()
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+
+    def _check_keyup_events(self, event):
+        """реагирует на отпускание клавиш"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False  # ВЫКЛЮЧАЕТ движение
+
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
+
+
+
 
 
 
--- ship.py
+++ ship.py
@@ -14,18 +14,28 @@ class Ship():
         self.rect.midbottom = self.screen_rect.midbottom  # выравнивание объекта по центру
 
         self.x = float(self.rect.x)  # сохранение вещественной координаты центра кораблчя. што блядь???
+        self.y = float(self.rect.y)  # см. коммент стр. 16
         self.moving_right = False  # флаг перемещения (клавиша нажата -> True, клавишу отпустили -> False)
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """Обновляет позицию корабля с учетом флага"""
+        # движение по горизонтали
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed  # логика перемещения
             # (+ вещественная координата центра корабля) + ограничение выхода за край экрана
         elif self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed  # см. ком. 23-24 стр.
+        # движение по вертикали
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
 
         self.rect.x = self.x  # обновление атрибута rect на основании self.x
+        self.rect.y = self.y  # дли движения по вертикали
 
     def blitme(self):
         """рисует корабль в текущей позиции"""
